d7798d3db1d2f9535f0620944df2b84b7cd255a2
재운재운 <0624jwjwjw@gmail.com>
2026-06-17 21:46:35 +0900

    기본 근접몬스터 제작
    몬스터 기능 : 플레이어 태그에 따라옴
    스킬 태그가 달려있는 오브잭트에 맞으면 반대로 튕겨나감 y축 기준으로 낮으면 레이어가 앞으로 보이게 함
    //나중에 플레이어에게도 y축기준 레이어 앞뒤 코드 추가 필요

---
+++ Assets/Scripts/Enumy/ShotRangeEnumyFollow.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotRangeEnumyFollow : MonoBehaviour
+{
+    [Header("몬스터 스텟")]
+    [SerializeField] private int Level = 1;
+    [SerializeField] private int EnumyHP = 3;
+    [SerializeField] private int EnumyATK = 1;
+    [SerializeField] private float EnumySpeed = 2.0f;
+    public float knockbackForce = 20.0f;
+
+    private Transform playerTransform;
+    private Rigidbody2D rb;
+    private bool isKnockedBack = false;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            playerTransform = playerObj.transform;
+        }
+    }
+
+    void FixedUpdate()
+    {
+       
+        if (isKnockedBack || playerTransform == null) return;
+
+        Vector2 direction = (playerTransform.position - transform.position).normalized;
+
+        rb.AddForce(direction * EnumySpeed * 10f);
+        if (rb.linearVelocity.magnitude > EnumySpeed)
+        {
+            rb.linearVelocity = rb.linearVelocity.normalized * EnumySpeed;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if (collision.CompareTag("Player"))
+        {
+            //Destroy(collision.gameObject);
+            //Debug.Log("죽어랏!");
+        }
+        if (collision.CompareTag("Skill"))
+        {
+            EnumyHP--;
+            StartCoroutine(KnockbackRoutine(collision.transform.position));
+            Debug.Log($"남은 체력 : {EnumyHP}");
+            if (EnumyHP<=0)
+            {
+                Destroy(gameObject);
+                Debug.Log("몬스터가 죽었다....");
+            }
+        }
+        
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enumy"))
+        {
+
+        }
+    }
+    private IEnumerator KnockbackRoutine(Vector3 attackerPos)
+    {
+        isKnockedBack = true;
+
+        Vector2 knockbackDir = (transform.position - attackerPos).normalized;
+        rb.linearVelocity = knockbackDir * knockbackForce;
+
+        yield return new WaitForSeconds(0.2f);
+
+        isKnockedBack = false;
+    }
+}
---
+++ Assets/Scripts/Enumy/SortingY.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class SortingY : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void LateUpdate()
+    {
+        int precision = 100;
+        spriteRenderer.sortingOrder = -(int)(transform.position.y * precision);
+    }
+}
