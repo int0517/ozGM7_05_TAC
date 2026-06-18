42b5b3213b3d1f70435cac0706b2a24252d4967e
김현아 <kimgusdk425@gmail.com>
2026-06-18 10:37:59 +0900

    Feat : UI레이아웃
    일시정지 틀 잡기

---
+++ Assets/Scripts/UI02/TextController.cs
@@ -0,0 +1,20 @@
+using TMPro;
+using Unity.VectorGraphics;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextController : MonoBehaviour
+{
+    public TextMeshProUGUI wave;
+    public TextMeshProUGUI attackPower;
+    public TextMeshProUGUI attackSpeed;
+    public TextMeshProUGUI moveSpeed;
+    public TextMeshProUGUI maxHP;
+    public TextMeshProUGUI magent;
+    public TextMeshProUGUI score;
+
+    private bool isBoss;
+    
+
+
+}
