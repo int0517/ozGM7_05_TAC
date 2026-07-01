f3d45fcf8e48e63587c3d881c3ba319db6a5a4b3
kkjs0517 <kkjs0517@gmail.com>
2026-07-01 10:39:37 +0900

    chore : 카메라 이동 제한
    카메라 화면이 맵을 안 벗어나게 제한

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -3,9 +3,19 @@
 public class CameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform target;
+    private Vector3 desPos;
+
+    [SerializeField] float desPosXMax, desPosXMin, desPosYMax, desPosYMin;
 
     void Update()
     {
-        transform.position = new Vector3(target.position.x, target.position.y, target.position.z - 10f);
+        desPos = new Vector3(target.position.x, target.position.y, target.position.z - 10f);
+
+        if (desPos.x < desPosXMin) desPos.x = desPosXMin;
+        if (desPos.x > desPosXMax) desPos.x = desPosXMax;
+        if (desPos.y < desPosYMin) desPos.y = desPosYMin;
+        if (desPos.y > desPosYMax) desPos.y = desPosYMax;
+
+        transform.position = desPos;
     }
 }
